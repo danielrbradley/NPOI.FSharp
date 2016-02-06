62f33664d59759b61eda873a52603a431f3dad10
Daniel Bradley <daniel@danielbradley.net>
2016-02-06 14:27:13 +0000

    Initialise project from ProjectScaffold

---
+++ src/NPOI.FSharp/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("NPOI.FSharp")>]
+[<assembly: AssemblyProductAttribute("NPOI.FSharp")>]
+[<assembly: AssemblyDescriptionAttribute("F# wrappers for NPOI")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
